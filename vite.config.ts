d1f6dbcac2b2be317e50eaf381a23c30e0347484
Agathe-Valence <agathe.valence@efrei.net>
2025-07-25 15:33:13 +0200

    MAJ

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/engineer-portfolio/', 
+  base: '/embedded-engineer/', 
   plugins: [react()]
 });
